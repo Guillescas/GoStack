6bfbf3fdca756e5a0059ec4f68fd47ed51cfc9db
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-17 23:13:38 -0300

    Nodemon added and server configs ended

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 
 const app = express()
 
-app.get('/projects', (req, res) => {
-  return res.status(200).send('Hello World!')
+app.get('/', (req, res) => {
+  return res.status(200).json({ message: 'Hello world!' })
 })
 
-app.listen(3333)
+app.listen(3333, () => {
+  console.log('🚀 Back-end started!')
+})
