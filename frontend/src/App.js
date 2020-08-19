bfcb0f36eeb1329454188f2873409ad44aec3fe3
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-19 00:00:32 -0300

    Connecting Front with Back-end (API)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,20 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import api from './services/api'
 
 import './App.css'
-import treesImage from './assets/trees.jpeg'
 
 import Header from './components/Header'
 
 function App() {
   const [projects, setProjects] = useState(['Desenvolvimento', 'web'])
 
+  useEffect(() => {
+    api.get('/projects').then(response => {
+      console.log(response)
+    })
+  }, [])
+
   function handleAddProject() {
-    // projects.push(`Novo Projeto ${Date.now()}`)
     setProjects([...projects, `Novo Projeto ${Date.now()}`])
 
     console.log(projects)
@@ -18,8 +23,6 @@ function App() {
   return (
     <>
       <Header title="Projects" />
-
-      <img width="400px" height="600px" src={treesImage} />
 
       <ul>
         {projects.map(project => <li key={project}>{project}</li>)}
