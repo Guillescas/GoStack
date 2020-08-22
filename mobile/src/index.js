34338e3e477778bc6989667b35b25d53e88fda28
Guilherme Illescas <gui.illescas@gmail.com>
2020-08-21 22:41:12 -0300

    List and add projects to/from the api

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { View, FlatList, Text, StyleSheet, StatusBar } from 'react-native'
+import { SafeAreaView, FlatList, Text, StyleSheet, StatusBar, TouchableOpacity } from 'react-native'
 
 import api from './services/api'
 
@@ -12,20 +12,40 @@ export default function App() {
     })
   }, [])
 
+  async function handleAddProject() {
+    const response = await api.post('projects', {
+      title: `Novo projeto ${Date.now()}`,
+      owner: 'Guilles'
+    })
+
+    const project = response.data
+
+    setProjects([...projects, project])
+  }
+
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
 
-      <FlatList
-        style={styles.container}
-        data={projects}
-        keyExtractor={project => project.id}
-        renderItem={({ item: project }) => (
-          <Text style={styles.project}>
-            {project.title}
-          </Text>
-        )}
-      />
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({ item: project }) => (
+            <Text style={styles.project}>
+              {project.title}
+            </Text>
+          )}
+        />
+
+        <TouchableOpacity 
+          activeOpacity={0.6} 
+          style={styles.button} 
+          onPress={handleAddProject}
+        >
+          <Text style={styles.buttonText}>Adicionar Projeto</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </>
   )
 }
@@ -38,6 +58,20 @@ const styles = StyleSheet.create({
 
   project: {
     color: '#fff',
-    fontSize: 20,
+    fontSize: 24,
+  },
+  
+  button: {
+    backgroundColor: '#fff',
+    margin: 20,
+    height: 50,
+    borderRadius: 4,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+
+  buttonText: {
+    fontWeight: 'bold',
+    fontSize: 10,
   },
 })
